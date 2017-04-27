20f7ef4106331560ccc0088b36650bfc4c527bb9
Matt Perl <mperl1@binghamton.edu>
2017-04-26 21:02:05 -0400

    Project framework

---
+++ Minigames/Minigames/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Minigames
+//
+//  Created by Matt Perl on 4/26/17.
+//  Copyright © 2017 Matt Perl, Joe Kim, Matt Spagnoli, Nick Seney. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Minigames/Minigames/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Minigames
+//
+//  Created by Matt Perl on 4/26/17.
+//  Copyright © 2017 Matt Perl, Joe Kim, Matt Spagnoli, Nick Seney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
