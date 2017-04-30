a6a1104b5dd1ffbc10ac2d13ab64487b6addf737
Matt Perl <mperl1@binghamton.edu>
2017-04-29 23:43:05 -0400

    Added UI elements, assets, and feilds to game4

--- Minigames/Minigames/GameFourViewController.h
+++ Minigames/Minigames/GameFourViewController.h
@@ -13,8 +13,9 @@
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSMutableArray *GridButtons;
 @property (strong, nonatomic) IBOutlet UITextField *CSTextField;
 @property (strong, nonatomic) IBOutlet UITextField *HSTextField;
+@property (strong, nonatomic) IBOutlet UITextField *remainingTriesField;
 
-
+@property (strong, nonatomic) IBOutlet UIButton *resetButton;
 @property (strong, nonatomic) IBOutlet UIButton *firstButtonPressed;
 @property (strong, nonatomic) IBOutlet UIButton *secondButtonPressed;
 
